00b8ce22948c144e16ebbed989c01d0236efbb07
Oleg <korotkiy.o@yandex.ru>
2021-10-11 17:10:10 +0300

    11/10/21

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -12,12 +14,40 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public Item[] findAll() {
+       return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rez = new Item[size];
+        int i = 0;
+        for (int index = 0; index < size; index++){
+            if(key.equals(items[index].getName())) {
+                rez[i++] = items[index];
+            }
+        }
+        return Arrays.copyOf(rez, i);
+    }
+
+    public boolean replace(int id, Item item) {
+      int index = indexOf(id);
+      if(index == -1){
+          return false;
+      }
+      item.setId(id);
+      items[index] = item;
+      return true;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
+            if(items[index].getId() == id) {
+                rsl = index;
             }
         }
         return rsl;
